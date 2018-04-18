a2614ddb478082aeba47a906815b3dc638cea848
Khaleel Younis <khaleelyounis1@gmail.com>
2018-04-18 16:15:49 -0700

    maxchar completed

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,20 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let chars = {};
+    let max = 0;
+    let charMax = '';
+    for (let character of str) {
+        chars[character] = chars[character] + 1 || 1;
+    }
+    for (var property in chars) {
+        if (chars[property] > max) {
+            max = chars[property];
+            charMax = property;
+        }
+    }
+    return charMax;
+}
 
 module.exports = maxChar;
