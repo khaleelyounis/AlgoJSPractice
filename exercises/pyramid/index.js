66ece68b86ae672e87e58d60938cda6b9655d20a
Khaleel Younis <khaleelyounis1@gmail.com>
2018-04-25 14:36:07 -0700

    pyramid solution 1

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,19 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+    const midpoint = Math.floor((2 * n - 1) / 2);
+    for (let row = 0; row < n; row++) {
+        let step = '';
+        for (let column = 0; column < n * 2 - 1; column++) {
+            if (midpoint - row <= column && midpoint + row >= column) {
+                step += '#';
+            } else {
+                step += ' ';
+            }
+        }
+        console.log(step);
+    }
+}
 
 module.exports = pyramid;
