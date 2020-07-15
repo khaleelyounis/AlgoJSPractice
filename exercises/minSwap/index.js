4a30384b4f0a6d19c2b7d274310e3acbbb6910ff
Khaleel Younis <Khaleelyounis.dev@gmail.com>
2020-07-15 14:17:54 -0700

    continued minSwap

--- exercises/minSwap/index.js
+++ exercises/minSwap/index.js
@@ -6,21 +6,46 @@
 // minSwap(s) = 2;
 // the index 0, 2 swap, the index 1,5 swap
 
+const determineValidString = (s) => {
+  if (!s.length) {
+    return false;
+  }
+
+  if (s.indexOf(')') < 0 || s.indexOf('(') < 0) {
+    return false;
+  }
+
+  let imbalanceCounter = 0;
+  const stringArr = s.split('');
+
+  for (let str of stringArr) {
+    if (str === '(') {
+      imbalanceCounter++;
+    } else if (str === ')') {
+      imbalanceCounter--;
+    } else {
+      return false;
+    }
+  }
+
+  return imbalanceCounter === 0;
+};
+
 const minSwap = (s) => {
+  if (!determineValidString(s)) {
+    return 'Invalid input string';
+  }
+
   const invalid_closed = [];
   const invalid_open = [];
 
   for (let i = 0; i < s.length; i++) {
     if (s[i] === '(') {
-      if (i === s.length - 1) {
-        invalid_open.push(i);
-      } else if (s[i + 1] !== ')') {
+      if (i === s.length - 1 || s[i + 1] !== ')') {
         invalid_open.push(i);
       }
     } else if (s[i] === ')') {
-      if (i === s.length - 1) {
-        invalid_closed.push(i);
-      } else if (s[i - 1] !== '(') {
+      if (s[i - 1] !== '(') {
         invalid_closed.push(i);
       }
     }
@@ -32,3 +57,5 @@ const minSwap = (s) => {
     return invalid_closed.length;
   }
 };
+
+module.exports = minSwap;
