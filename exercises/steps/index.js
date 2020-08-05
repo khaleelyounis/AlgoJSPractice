051bc7f49edabe0e168eec58c3655905fce9662c
khaleelyounis <Khaleelyounis.dev@gmail.com>
2020-08-05 15:56:18 -0700

    added new steps solution

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,22 +17,32 @@
 //       '### '
 //       '####'
 
-function steps(n, row = 0, stair = '') {
-  if (n === row) {
+// function steps(n, row = 0, stair = '') {
+//   if (n === row) {
+//     return;
+//   }
+
+//   if (n === stair.length) {
+//     console.log(stair);
+//     return steps(n, row + 1);
+//   }
+
+//   if (stair.length <= row) {
+//     stair += '#';
+//   } else {
+//     stair += ' ';
+//   }
+//   steps(n, row, stair);
+// }
+
+function steps(n) {
+  if (!n) {
     return;
   }
 
-  if (n === stair.length) {
-    console.log(stair);
-    return steps(n, row + 1);
+  for (let i = 1; i <= n; i++) {
+    console.log('#'.repeat(i) + ' '.repeat(n - i));
   }
-
-  if (stair.length <= row) {
-    stair += '#';
-  } else {
-    stair += ' ';
-  }
-  steps(n, row, stair);
 }
 
 module.exports = steps;
